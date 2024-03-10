62132176015a2b40fd3a09718f60c31d943410f3
Saad-Eddine-Omary <saad.eddine.omary12@gmail.com>
2024-03-10 13:52:40 +0100

    added tests for args

--- src/controller/args.rs
+++ src/controller/args.rs
@@ -37,3 +37,58 @@ pub fn parse_arguments() -> Arguments {
     Arguments::parse()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_arguments() {
+        // Simulate command-line arguments
+        let args = vec![
+            "program_name",
+            "--load-balancer-ip",
+            "192.168.1.196:5555",
+            "-b",
+            "192.168.1.192:2222,192.168.1.190:8080,192.168.1.191:3333",
+            "-p",
+            "/health-check",
+            "-i",
+            "3",
+            "-s",
+            "3",
+            "-r",
+            "5",
+        ];
+
+        // Parse the arguments
+        let parsed_args = Arguments::parse_from(args.iter());
+
+        // Expected values
+        let expected_args = Arguments {
+            load_balancer_ip: "192.168.1.196:5555".to_string(),
+            health_check_path: Some("/health-check".to_string()),
+            health_check_interval: Some(3),
+            bind: vec![
+                "192.168.1.192:2222".to_string(),
+                "192.168.1.190:8080".to_string(),
+                "192.168.1.191:3333".to_string(),
+            ],
+            window_size_secs: 3,
+            max_requests: 5,
+        };
+
+        // Verify that the parsed arguments match the expected values
+        assert_eq!(parsed_args.load_balancer_ip, expected_args.load_balancer_ip);
+        assert_eq!(
+            parsed_args.health_check_path,
+            expected_args.health_check_path
+        );
+        assert_eq!(
+            parsed_args.health_check_interval,
+            expected_args.health_check_interval
+        );
+        assert_eq!(parsed_args.bind, expected_args.bind);
+        assert_eq!(parsed_args.window_size_secs, expected_args.window_size_secs);
+        assert_eq!(parsed_args.max_requests, expected_args.max_requests);
+    }
+}
