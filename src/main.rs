53f3f371a2041f5ad233da88bc5522cecced6b72
Quentin Hélion <helion.quentin@gmail.com>
2024-03-10 13:41:20 +0100

    clean main

--- src/main.rs
+++ src/main.rs
@@ -70,26 +70,4 @@ async fn main() {
 
     println!("Listening on 127.0.0.1:8080...");
 
-
-    // while let Ok((stream, _)) = listener.accept() {
-    //     let stream_buff_err = stream.try_clone().expect("Failed to clone stream");
-    //     let stream_buff_res = stream.try_clone().expect("Failed to clone stream");
-    //     let gen_resp = generator("200 OK", "text/plain", "Hello world ");
-    //     task::spawn(async {
-    //         let result = handle_client(stream);
-
-    //         // Parse the HTTP request string
-    //         match HttpRequest::from_string(result) {
-    //             Ok(parsed_request) => {
-    //                 println!("Parsed HTTP Request: {:#?}", parsed_request);
-    //             }
-    //             Err(err) => {
-    //                 make_http_error(err, stream_buff_err);
-    //                 eprintln!("Error parsing HTTP request: {}", err);
-    //             }
-    //         }
-    //         write_to_stream(gen_resp, stream_buff_res);
-    //     });
-        
-    // }
 }
